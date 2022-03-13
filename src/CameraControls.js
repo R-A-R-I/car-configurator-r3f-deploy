85871bd30e3265d75cccd756e6a3545dcec4934d
R-A-R-I <roryallen58@gmail.com>
2022-03-12 19:23:25 -0500

    Cleaned up the state object

--- src/CameraControls.js
+++ src/CameraControls.js
@@ -1,16 +1,19 @@
 import * as THREE from "three";
 import {useFrame} from '@react-three/fiber';
-import State from "./State";
+import state from "./State";
 
 const CameraControls = ({})=>{
 
+    const vec = new THREE.Vector3();
 
-    useFrame(({camera})=>{
+    useFrame(({camera,scene})=>{
         //extract the camera by destructuring the parameter passed to the callback of the useFrame. Its similar to destructing useThree but w/o the hook
 
-        console.log(camera)
-        camera.position.lerp(State.cameraPos, 0.1)
+        //console.log(camera)
+
+        camera.position.lerp(vec.set(...state.cameraPos), 0.1)
         //position is a vector3 so you can find further function for position by using vector3 ie lerp
+        scene.orbitControls.update()
     })
 
    
--- src/State.js
+++ src/State.js
@@ -1,9 +1,15 @@
 import * as THREE from "three";
 
 
-const State ={
+const state ={
+    /****THREE was imported because you need vectors and THREE happens to have vector objects that you can use
     cameraPos:new THREE.Vector3(6,3,5),
-    targetPos:new THREE.Vector3(-6,3,5)
+    targetPos:new THREE.Vector3(-6,3,5) */
+    
+    /****Alternately(Cleaner) you can convert the array value vector3 using "set" and "spread" the array values to match the parameters in the CameraControls file**/
+    cameraPos:[6,3,5],
+    targetPos:[-6,3,5]
+
 }
 
-export default State;
+export default state;
